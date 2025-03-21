25d84bdaf94a5d9229aa1489838988c5ac6b2cf1
E090154 <maxime.gaspard_ext@michelin.com>
2025-03-21 09:32:21 +0000

    delete - from correction azcli

--- Correction/Azcli/mission4.sh
+++ Correction/Azcli/mission4.sh
@@ -1,10 +1,8 @@
 az backup vault create --name EarthBackupVault --location francecentral --resource-group EarthCommand_RG --sku Standard
 
-------------------------------------------------------------------------------------------------------------
 
 az backup policy create --name EarthVMBackupPolicy --vault-name EarthBackupVault --resource-group EarthCommand_RG --backup-management-type AzureIaasVM --policy "$(az backup policy get-default --vault-name EarthBackupVault --resource-group EarthCommand_RG --query properties -o json | jq '.schedulePolicy.scheduleRunFrequency="Daily" | .schedulePolicy.scheduleRunTimes=["2023-10-21T12:00:00Z"] | .retentionPolicy.dailySchedule.retentionDurationCount=7')"
 
-------------------------------------------------------------------------------------------------------------
 
 vm_id=$(az vm show --name EarthVM --resource-group EarthCommand_RG --query id -o tsv)
 
